95ac7e05d356ba5baaf9a1967764a4056892fcb2
Reimu NotMoe <reimu@sudomaker.com>
2023-06-06 17:53:59 +0800

    update

--- target/loshark_v2.2/main.cpp
+++ target/loshark_v2.2/main.cpp
@@ -4,9 +4,8 @@
     Author: Reimu NotMoe <reimu@sudomaker.com>
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+    it under the terms of version 3 of the GNU Affero General Public License
+    as published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,31 +17,63 @@
 */
 
 /*
+    Additional permissions under section 7 of the GNU AGPLv3 license
+
+
+    1. Definitions
+
+    In this document, the following terms shall have the following meanings:
+
+    "Hardware devices" refers to physical electronic components that require
+    electricity to operate, including but not limited to computers, phones,
+    tablets, servers, routers, switches, modems, printers, scanners, cameras,
+    microphones, speakers, thumb drives, NFC tags, smart cards, add-in cards,
+    gadgets, torch lights, power banks, fans, washing machines, microwaves,
+    air conditioners, multi-meters, oscilloscopes, logic analyzers, television
+    sets, multimedia players, radio transmitters, drones, model airplanes,
+    robots, PDAs, gaming consoles and cartridges, smartwatches, wristbands,
+    GNSS navigators, electrical music instruments, bare printed circuit boards
+    with or without a specific purpose, and other similar devices.
+
+    "GNU AGPLv3" or "AGPLv3" refers to version 3 of the GNU Affero General
+    Public License published by the Free Software Foundation.
+
+    "Executable" refers to a type of computer file that contains a program or set
+    of instructions that can be run by a computer's operating system or processor.
+
+    "SudoMaker" refers to SudoMaker, Ltd.
+
+    Definitions under section 0 of the AGPLv3 is also applicable here.
+
+
+    2. Additional Permissions
+
     SudoMaker, Ltd. hereby grants you additional permissions under section 7 of
     the GNU Affero General Public License version 3 ("AGPLv3") in accordance
     with the following terms:
 
-    1. You are permitted to modify and distribute this program or any
-    covered work without being required to distribute the source code of your
-    modifications, provided that the resulting executable (binary code) is
-    exclusively designed and intended to run on (a) hardware devices
-    manufactured by SudoMaker, Ltd., (b) hardware components manufactured by
-    SudoMaker, Ltd. which are parts of a larger hardware device, or (c)
-    hardware devices that are certified through the "Resonance Certification
-    Program" by SudoMaker, Ltd.
+    You are permitted to modify and distribute this program or any covered work
+    without being required to distribute the source code of your modifications,
+    provided that the resulting executable or object code is exclusively
+    designed and intended to run on (a) hardware devices manufactured by
+    SudoMaker, Ltd., (b) hardware devices manufactured by SudoMaker, Ltd. which
+    are used as components of a larger hardware device, or (c) hardware devices
+    that are certified through the "Resonance Certification Program" by
+    SudoMaker, Ltd.
 
-    2. This additional permission is granted solely for the purpose of enabling
+    This additional permission is granted solely for the purpose of enabling
     the use of this program on the aforementioned hardware devices or
     components, and does not extend to any other use or distribution of this
     program.
 
-    3. If you distribute or make available any executable (binary code) that
+    If you distribute or make available any executable or object code that
     was modified under the terms of this additional permission, you must state
     that it is distributed or made available solely for use on the
     aforementioned hardware devices or components.
 
-    4. These additional permissions are granted to you in conjunction with the
-    AGPLv3 and does not affect other rights or obligations under that license.
+    These additional permissions are granted to you in conjunction with the
+    GNU AGPLv3 license and does not affect other rights or obligations under
+    that license.
 
     By exercising any of the additional permissions granted here, you agree to
     be bound by the terms and conditions mentioned above.
@@ -85,7 +116,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	assert(0 == mount_it(cl_root, "/new_root", "jfs"));
+	assert(0 == mount_it(cl_root, "/new_root", "jfs", MS_RDONLY));
 
 	assert(0 == umount_big_three("/"));
 
